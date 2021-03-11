a826db738ef8a284dfaa41af267f3b16199b91b6
Sarikaya20 <80039204+Sarikaya20@users.noreply.github.com>
2021-03-11 11:38:07 +0100

    View class

--- src/Cantons_View.java
+++ src/Cantons_View.java
@@ -1,8 +1,30 @@
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Cantons_View {
+	
+	private final Cantons_Model model;
+	private Stage stage;
+	
+	protected Cantons_View(Stage stage, Cantons_Model model) {
+		this.stage = stage;
+		this.model = model;
+		stage.setTitle("Kantönli");
+		
+		BorderPane pane = new BorderPane();
+		
+		VBox vbox = new VBox();
+		
+		pane.setCenter(vbox);
+		
+		
+		
+	}
 
 	public void start() {
-		// TODO Auto-generated method stub
+		
+		stage.show();
 		
 	}
 
