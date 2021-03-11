33db8b88270596cf6c62d9324e43820bff728db0
IvanDePaola <ivan.depaola@students.fhnw.ch>
2021-03-11 09:52:18 +0100

    MVC erstellt + Hauptklasse angepasst

--- src/Cantons.java
+++ src/Cantons.java
@@ -1,4 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Cantons {
+public class Cantons extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
---
+++ src/Cantons_Model.java
@@ -0,0 +1,4 @@
+
+public class Cantons_Model {
+
+}
