14a3888a0e5beec59637a2c15353f8860ab187ec
YourHighness <levi.kammermann@me.com>
2021-03-11 11:13:50 +0100

    commit

---
+++ src/CantonNames.java
@@ -0,0 +1,4 @@
+
+public enum CantonNames {
+
+}
--- src/Cantons.java
+++ src/Cantons.java
@@ -2,11 +2,29 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Cantons extends Application {
+	private Kanton_Model model;
+	private Kanton_View view;
+	private Kanton_Controller controller;
+
+	public static void main(String[] args) {
+		launch();
+
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		model = new Kanton_Model ();
+		view = new Kanton_View(primaryStage, model);
+		controller = new Kanton_Controller(model, view);
+		
+		view.start();
+		
+		
+		
+		
+		
+		
+		
 		
 	}
-
 }
